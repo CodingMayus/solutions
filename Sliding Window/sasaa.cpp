81503bf12d3e1cb8de5a32a480e3e95a9037f58e
CodingMayus <106496765+CodingMayus@users.noreply.github.com>
2023-08-13 15:43:22 -0400

    Add files via upload

---
+++ Sliding Window/sasaa.cpp
@@ -0,0 +1,47 @@
+// Source: https://usaco.guide/general/io
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int mx = 1e5 +5;
+ll a[2*mx],prefix[2*mx];
+int main() {
+int n,k;
+cin>>n>>k;
+for(int i=1;i<=n;i++){
+cin>>a[i];
+a[i+n]=a[i];
+}
+// for(int i=1;i<=2*n;i++){
+// 	//cout<<a[i]<<endl;
+// }
+for(int i=1;i<=2*n;i++){
+		prefix[i]=prefix[i-1]+a[i];
+	//	cout<<prefix[i]<<endl;
+		//cout<<prefix[i]<<"\n";
+}
+ll ans=-INT_MAX;
+deque<ll>poop;
+for(int i=1;i<=k;i++){
+	while(!poop.empty()&&prefix[poop.back()]<prefix[i]){
+		poop.pop_back();
+	}
+	poop.push_back(i);
+}
+for(int i=k+1;i<=2*n;i++){
+ans=max(ans,prefix[poop.front()]-prefix[i-k-1]);
+//cout<<poop.front()<<"\n";
+if(!poop.empty()&&poop.front()<=i-k){
+	poop.pop_front();
+}
+while(!poop.empty()&&prefix[poop.back()]-prefix[i-k]<prefix[i]-prefix[i-k]){
+poop.pop_back();
+}
+poop.push_back(i);
+}
+ans=max(ans,prefix[poop.front()]-prefix[2*n-k]);
+
+cout<<ans<<"\n";
+//sliding window
+
+}
