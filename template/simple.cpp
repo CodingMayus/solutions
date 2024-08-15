7c25a3b88dd593639c7ef367609b3aa733eeaa05
Matthew Yu <106496765+CodingMayus@users.noreply.github.com>
2024-08-15 02:29:31 -0400

    Create simple.cpp

---
+++ template/simple.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#define pb push_back
+#define pf push_front
+
+
+
+
+int main() {
+cin.tie(0);
+cin.sync_with_stdio(0);
+
+
+
+}
